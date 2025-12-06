e05ae8fe477bc66581f883a150951e963633b16a
valunix <210689854+valunix@users.noreply.github.com>
2025-12-06 18:04:23 +0100

    wip

---
+++ tests/Nuons.CodeInjection.Abstractions.Tests/AttributeConditionalTests.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using System.Reflection;
+using Nuons.Core.Abstractions;
+
+namespace Nuons.CodeInjection.Abstractions.Tests;
+
+public class AttributeConditionalTests
+{
+	[Fact]
+	public void AllCustomAttributes_HaveConditionalAttribute_WithCorrectCondition()
+	{
+		// Arrange
+		var assembly = typeof(CodeInjectionAbstractionsAssemblyMarker).Assembly;
+		var attributeTypes = assembly.GetTypes()
+			.Where(type => type.IsSubclassOf(typeof(Attribute)))
+			.Where(type => type.Namespace is not null && type.Namespace.StartsWith("Nuons"))
+			.ToList();
+
+		// Assert
+		attributeTypes.Count.ShouldBeGreaterThan(0, "No custom attributes found in the assembly");
+
+		// Act
+		foreach (var attributeType in attributeTypes)
+		{
+			var conditionalAttribute = attributeType.GetCustomAttribute<ConditionalAttribute>();
+
+			// Assert
+			conditionalAttribute.ShouldNotBeNull($"{attributeType.Name} should have ConditionalAttribute");
+			conditionalAttribute.ConditionString.ShouldBe(
+				Constants.CodeGenerationCondition,
+				$"{attributeType.Name} should have ConditionalAttribute with condition '{Constants.CodeGenerationCondition}'");
+		}
+	}
+}
