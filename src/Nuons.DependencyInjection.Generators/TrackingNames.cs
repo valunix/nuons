11d2e511787c123968dff7dc5bd0fad76a92b6b5
valunix <210689854+valunix@users.noreply.github.com>
2026-03-12 21:37:10 +0100

    wip

---
+++ src/Nuons.DependencyInjection.Generators/TrackingNames.cs
@@ -0,0 +1,10 @@
+namespace Nuons.DependencyInjection.Generators;
+
+internal static class TrackingNames
+{
+	public const string AssemblyName = nameof(AssemblyName);
+	public const string TransientProvider = nameof(TransientProvider);
+	public const string ScopedProvider = nameof(ScopedProvider);
+	public const string SingletonProvider = nameof(SingletonProvider);
+	public const string AllRegistrations = nameof(AllRegistrations);
+}
